4709301237cca3956ac3c651ee1df76551601728
Harsh Bediya <harshbedi3112@gmail.com>
2026-09-23 10:30:15 +0530

    Add examples of comparison, logical, and ternary operators in Oprator.js

--- JavaScript/Oprator.js
+++ JavaScript/Oprator.js
@@ -1,8 +1,39 @@
-let x=5;
-console.log(--x);
-console.log(x++);
-console.log(++x);
-console.log(x--);
-console.log(++x);
-console.log(x++);
-console.log(++x);
+// let x=5;
+// console.log(--x);
+// console.log(x++);
+// console.log(++x);
+// console.log(x--);                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                   
+// console.log(++x);
+// console.log(x++);
+// console.log(++x);
+
+let a=10;
+let b=2;
+let c="10";
+
+console.log(a==c); //it will only chack value not data type
+console.log(a===c); //it will only chack both value data type
+
+console.log(a!=b);
+console.log(a!==c);
+
+console.log(a<b);
+console.log(a>b);
+
+// Logical AND (&&) Logical OR (||) Logical NOT (!)
+console.log(a>b && b>c);
+console.log(a>b || b>c);
+
+console.log(a<=c);
+console.log(a<=b);
+console.log(a<=c); // it's
+//  compre with value that's way is true 
+
+// ternary oprator
+ let age =18
+ let result= (age>=18) ? "You can vote":"YOu can not vote"
+ console.log(result)
+
+ //Assignment operator
+ 
+ //? => =, +=, -= , /= , %= , *= , **=
